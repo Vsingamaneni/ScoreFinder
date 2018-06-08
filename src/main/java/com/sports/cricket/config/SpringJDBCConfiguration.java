02f1fce3c14f539c33aacd2f56901740abe3e940
Vamsi Singamaneni <vamsi.singamaneni0@walmart.com>
2018-06-08 00:59:52 -0500

    Updated db details

--- src/main/java/com/sports/cricket/config/SpringJDBCConfiguration.java
+++ src/main/java/com/sports/cricket/config/SpringJDBCConfiguration.java
@@ -10,24 +10,30 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class SpringJDBCConfiguration {
+
+    // Website login Details
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        //MySQL database we are using
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        //Working
-        //dataSource.setUrl("jdbc:mysql://node73943-scorefinder.whelastic.net/sports");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/sports");
-        // Google cloud URL - 35.194.225.158
-        //dataSource.setUrl("jdbc:mysql://35.201.185.22:3306/sports");
-        //dataSource.setUrl("jdbc:mysql://google/sports?cloudSqlInstance=scorefinder-206414:asia-east1:scorefinderapp&amp;socketFactory=com.google.cloud.sql.mysql.SocketFactory&amp;");
-        //dataSource.setUrl("jdbc:mysql://google/sports?cloudSqlInstance=scorefinderapp&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=vamsi&password=rooney&useSSL=false");
+        dataSource.setUrl("jdbc:mysql://node73943-scorefinder.whelastic.net/sports");
         dataSource.setUsername("root");
-        //dataSource.setPassword("BNIers16706");
-        dataSource.setPassword("");
+        dataSource.setPassword("BNIers16706");
 
         return dataSource;
     }
+
+    // Local MySQL details
+    /*@Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sports");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }*/
 
     @Bean
     public JdbcTemplate jdbcTemplate() {
